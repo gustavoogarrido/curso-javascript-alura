e8669b5d16ac15f04ae154db1fd3efaeb0aa25c0
Gustavo <gustavoog05@hotmail.com>
2026-01-13 20:16:27 -0300

    Finishing class 2

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 alert('Você terá que tentar adivinhar o número secreto que está entre 1 e 100. Boa sorte!');
 let secret = 32;
-
+let x = 0
 let resposta = prompt('Digite o seu palpite, deve ser apenas um número');
 while (resposta != secret) {
+    x += 1
+    console.log(`Iteração numero ${x}`)
     if (resposta > secret) {
         alert('Seu palpite foi muito alto, tente algo menor');
     }
@@ -12,7 +14,7 @@ while (resposta != secret) {
     resposta = prompt('Digite o seu palpite, deve ser apenas um número');
 }
 
-// Desafio
+// Desafios aula 1
 
 alert('Boas vindas ao nosso site!')
 
@@ -28,3 +30,23 @@ alert(mensagemErro)
 
 nome = prompt('Digite o nome')
 idade = prompt('Digite a idade') >= 18 ? alert('Pode tirar habilitação') : alert('Não pode tirar habilitadação') //ternario é muito bom
+
+// Desafios aula 2
+
+// 1-
+let respostaDiaSem = prompt('Que dia é hoje?')
+const finalDeSemana = ['sábado', 'domingo']
+if (finalDeSemana.includes(respostaDiaSem.toLowerCase().normalize('NFD')))
+    alert('Bom fim de semana')
+
+else 
+    alert('Boa semana para você')
+
+// 2-
+
+let feedback = Number(prompt('Digite um número')) > 0 ? 'O número é positivo' : 'O número é negativo'
+
+if (numeroDigitado > 0)
+    alert('O número é positivo')
+else
+    alert('O número é negativo')
