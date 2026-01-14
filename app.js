82c1b4a5e7290d61b0282872b90ed596d224d1ca
Gustavo <gustavoog05@hotmail.com>
2026-01-13 22:16:18 -0300

    Finishing class 4

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+// Função da documentação
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 alert('Você terá que tentar adivinhar o número secreto que está entre 1 e 100. Boa sorte!');
-let secret = 32;
+let secret = parseInt(Math.random() * 100 + 1);
 let x = 0
 let resposta = prompt('Digite o seu palpite, deve ser apenas um número');
 while (resposta != secret) {
     x++
     console.log(`Iteração numero ${x}`)
+    console.log(`O secret é ${secret}`)
     if (resposta > secret) {
         alert('Seu palpite foi muito alto, tente algo menor');
     }
@@ -46,7 +54,7 @@ else
 
 // 2-
 
-let feedback = Number(prompt('Digite um número')) > 0 ? 'O número é positivo' : 'O número é negativo'
+let feedback = Number(prompt('Digite um número')) > 0 ? 'O número é positivo' : 'O número é negativo' // Conteúdo da aula 4
 
 if (numeroDigitado > 0)
     alert('O número é positivo')
@@ -58,3 +66,22 @@ else
 let count
 for (count = 0; count < 10; count++)
     console.log(`Iteração número ${count}`)
+
+//Desafios aula 4 (finais)
+
+console.log('Começando desafios finais');
+let nome4 = "Gustavo"
+
+console.log(`Meu nome é ${nome4}`)
+
+alert(`O nome é ${nome4}`)
+
+const respostaLinguagem = prompt('Qual a linguagem de programação que você mais gosta?')
+console.log(respostaLinguagem)
+
+const valor1 = 324
+const valor2 = 654
+const resultado = valor1 + valor2
+
+console.log(`${valor1} + ${valor2} = ${resultado}`)
+console.log(`${valor1} - ${valor2} = ${valor1 - valor2}`)
